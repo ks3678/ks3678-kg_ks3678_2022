af703c2cbb4d9fe8b1c29e3868d3cff9d3bc4634
Karl Sun <ks3678@ad.rit.edu>
2021-05-02 21:30:53 -0400

    compared digit with dictionary key value
    Compared each single digit with pre-coded dictionary. If digit matches up with dictionary key, then add the keys value to the result string. Finally return result string.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-let input = [1, 10, 123];
+let input = [1, 2561, 123];
 
 let dict = {
+    0:"Zero",
     1:"One",
     2:"Two",
     3:"Three",
@@ -25,6 +26,32 @@ function phonetic(array){
         for(let j = 0; j<temp.length; j++)
         {
             integerSplit.push(temp.charAt(j));
+        }
+
+        //adds a space to seperate complete ints (ex. 1, 10 -> '1',' ','1','0);
+        integerSplit.push(" ");
+    }
+
+    console.log(integerSplit);
+
+    //compares each digit with dictionary key
+    for(let i =0; i<integerSplit.length; i++)
+    {
+        //add comma between each complete ints
+        if(integerSplit[i] == " "){
+            result += ",";
+        }
+        else{
+            for(let key in dict)
+        {
+            //converts char back to int
+            //compares int with dictionary key
+            //if key equals int then the value to result
+            if(key == parseInt(integerSplit[i]))
+            {
+                result += dict[key];
+            }
+        }
         }
     }
 
